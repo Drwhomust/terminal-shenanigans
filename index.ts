4f08d4c27b463dc2b62e926bd2bd4ef9b519d824
Drwhomust <rabbids8008@gmail.com>
2026-05-08 14:28:20 -0700

    bleh

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+// funny cat
+// get infected by the funny wacky cat catte from kaiju paradise or else...
+
+console.log("Hello World!");
+
+// pak ages
+import chalk from "chalk";
+import prompts from "prompts";
+
+// -- varibales
+
