8389cfc4612efe573f2c6a04d594d7a99f4db413
Drwhomust <rabbids8008@gmail.com>
2026-05-08 17:00:20 -0700

    new stuff, still testing

--- index.ts
+++ index.ts
@@ -5,7 +5,20 @@ console.log("Hello World!");
 
 // pak ages
 import chalk from "chalk";
-import prompts from "prompts";
+const prompts = require('prompts');
 
 // -- varibales
+let parmra1:any;
+let commands:string[] = ["Test", "About", "Commands Available"];
 
+// functions
+
+function test() {
+    console.log("Hello World!");
+}
+
+function cmd_available() {
+    console.log(chalk.greenBright(commands));
+}
+
+cmd_available();
